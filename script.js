1504edfe7905d5f99ec639c962894de6375a42b7
Benja-guinle <136008709+Benja-guinle@users.noreply.github.com>
2023-07-04 15:46:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function escribirMensajes(){
+    if (confirm ("¿te gusta programar?")){
+
+        alert(" a bueno entoces eres un niño rata ")
+}
+
+else{
+    alert("no eres bienvenido");      
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
